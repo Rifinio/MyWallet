ede04d6073e8841037b6a859fd02284a228e1d76
Adil BOUGAMZA <rifinio88@gmail.com>
2019-09-15 20:14:09 +0200

    Create TransactionViewModel

---
+++ MyWallet/ViewModel/TransactionViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionViewModel.swift
+//  MyWallet
+//
+//  Created by Adil Bougamza on 15/09/2019.
+//  Copyright © 2019 Adil Bougamza. All rights reserved.
+//
+
+import Foundation
+
+class TransactionViewModel {
+    var transaction: Transaction
+    init(transaction: Transaction) {
+        self.transaction = transaction
+    }
+}
