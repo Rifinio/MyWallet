30db5dae058c688116b1b4d13d4f5873a7e77c93
Adil BOUGAMZA <rifinio88@gmail.com>
2019-09-15 14:00:08 +0200

    Create AccountViewModel to hold business logic for the AccountViewController

---
+++ MyWallet/ViewModel/AccountViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AccountViewModel.swift
+//  MyWallet
+//
+//  Created by Adil Bougamza on 15/09/2019.
+//  Copyright © 2019 Adil Bougamza. All rights reserved.
+//
+
+import Foundation
+
+class AccountViewModel {
+    
+}
