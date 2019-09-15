d0da4120370dbc07343655ea885badd7486337f2
Adil BOUGAMZA <rifinio88@gmail.com>
2019-09-15 13:59:26 +0200

    Add LoadAccount method to load the data from json file.

---
+++ MyWallet/Factory/DataStore.swift
@@ -0,0 +1,27 @@
+//
+//  DataStore.swift
+//  MyWallet
+//
+//  Created by Adil Bougamza on 15/09/2019.
+//  Copyright © 2019 Adil Bougamza. All rights reserved.
+//
+
+import Foundation
+
+class DataStore {
+    func loadAccount() -> Account? {
+        if let path = Bundle.main.path(forResource: "transactions", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+                let decoder = JSONDecoder()
+                if let account = try? decoder.decode(Account.self, from: data) {
+                    return account
+                } else {
+                    return nil
+                }
+            } catch { return nil }
+        }
+        
+        return nil
+    }
+}
